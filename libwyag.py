fd449c5243e0958abeaaba1c68242d00ed1ccfde
jacobrluttrull <jacobrluttrull@gmail.com>
2026-01-05 20:18:21 -0500

    fixed errors and completed git project :)

--- libwyag.py
+++ libwyag.py
@@ -224,8 +224,9 @@ argsp.add_argument("object", metavar="object", help="The object to display")
 
 def cmd_cat_file(args):
     repo = repo_find()
-    cat_file(repo, args.object, args.type.encode())
-def cat_file(repo, objname, fmt):
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
     obj = object_read(repo, object_find(repo, obj, fmt=fmt))
     sys.stdout.buffer.write(obj.serialize())
 def object_find(repo, name, fmt=None, follow=True):
@@ -256,14 +257,8 @@ def cmd_hash_object(args):
     else:
         repo = None
     with open(args.path, "rb") as f:
-        data = f.read()
-        match fmt:
-            case b"commit": obj = GitCommit(data)
-            case b"tree"  : obj = GitTree(data)
-            case b"tag"   : obj = GitTag(data)
-            case b"blob"  : obj = GitBlob(data)
-            case _: raise Exception(f"Unknown type {fmt}!")
-        return object_write(obj, repo)
+        sha = object_hash(f, args.type.encode(), repo)
+        print(sha)
 def object_hash(fd, fmt, repo=None):
     """Hash Object, writing it to the repo if provided"""
     data = fd.read()
@@ -1228,3 +1223,190 @@ def rm(repo, paths, delete=True, skip_missing=False):
     # Update the list of entries in the index, and write it back.
     index.entries = kept_entries
     index_write(repo, index)
+
+## 9.3 the add command
+
+argsp = argsubparsers.add_parser("add", help="Add file contents to the index.")
+argsp.add_argument("path", nargs="+", help="Files to add to the index.")
+
+def cmd_add(args):
+    repo = repo_find()
+    add(repo, args.path)
+
+
+
+def add(repo, paths, delete=True, skip_missing=False):
+
+    # First remove all paths from the index, if they exist.
+    rm (repo, paths, delete=False, skip_missing=True)
+
+    worktree = repo.worktree + os.sep
+
+    # Convert the paths to pairs: (absolute, relative_to_worktree).
+    # Also delete them from the index if they're present.
+    clean_paths = set()
+    for path in paths:
+        abspath = os.path.abspath(path)
+        if not (abspath.startswith(worktree) and os.path.isfile(abspath)):
+            raise Exception(f"Not a file, or outside the worktree: {paths}")
+        relpath = os.path.relpath(abspath, repo.worktree)
+        clean_paths.add((abspath,  relpath))
+
+    # Find and read the index.  It was modified by rm.  (This isn't
+    # optimal, good enough for wyag!)
+    #
+    # @FIXME, though: we could just move the index through
+    # commands instead of reading and writing it over again.
+    index = index_read(repo)
+
+    for (abspath, relpath) in clean_paths:
+        with open(abspath, "rb") as fd:
+            sha = object_hash(fd, b"blob", repo)
+
+            stat = os.stat(abspath)
+
+            ctime_s = int(stat.st_ctime)
+            ctime_ns = stat.st_ctime_ns % 10**9
+            mtime_s = int(stat.st_mtime)
+            mtime_ns = stat.st_mtime_ns % 10**9
+
+            entry = GitIndexEntry(ctime=(ctime_s, ctime_ns), mtime=(mtime_s, mtime_ns), dev=stat.st_dev, ino=stat.st_ino,
+                                  mode_type=0b1000, mode_perms=0o644, uid=stat.st_uid, gid=stat.st_gid,
+                                  fsize=stat.st_size, sha=sha, flag_assume_valid=False,
+                                  flag_stage=False, name=relpath)
+            index.entries.append(entry)
+
+    # Write the index back
+    index_write(repo, index)
+
+#9.4 commit command
+
+argsp = argsubparsers.add_parser("commit", help="Create a new commit object.")
+argsp.add_argument("-m", dest="message", required=True, help="The commit message.")
+
+def gitconfig_read():
+    xdg_config_home = os.environ.get("XDG_CONFIG_HOME") if "XDG_CONFIG_HOME" in os.environ else "~/.config"
+    config_files = [
+        os.path.join(xdg_config_home, "git/config"),
+        os.path.expanduser("~/.gitconfig"),
+    ]
+
+    config = configparser.ConfigParser()
+    config.read(config_files)
+    return config
+
+def gitconfig_user_get(config):
+    if "user" in config:
+        if "name" in config["user"] and "email" in config["user"]:
+            return f"{config['user']['name']} <{config['user']['email']}>"
+    return None
+
+
+def tree_from_index(repo, index):
+    contents = dict()
+    contents[""] = list()
+
+    # Enumerate entries, and turn them into a dictionary where keys
+    # are directories, and values are lists of directory contents.
+    for entry in index.entries:
+        dirname = os.path.dirname(entry.name)
+
+        # We create all dictonary entries up to root ("").  We need
+        # them *all*, because even if a directory holds no files it
+        # will contain at least a tree.
+        key = dirname
+        while key != "":
+            if not key in contents:
+                contents[key] = list()
+            key = os.path.dirname(key)
+
+        # For now, simply store the entry in the list.
+        contents[dirname].append(entry)
+
+    # Get keys (= directories) and sort them by length, descending.
+    # This means that we'll always encounter a given path before its
+    # parent, which is all we need, since for each directory D we'll
+    # need to modify its parent P to add D's tree.
+    sorted_paths = sorted(contents.keys(), key=len, reverse=True)
+
+    # This variable will store the current tree's SHA-1.  After we're
+    # done iterating over our dict, it will contain the hash for the
+    # root tree.
+    sha = None
+
+    # We ge through the sorted list of paths (dict keys)
+    for path in sorted_paths:
+        # Prepare a new, empty tree object
+        tree = GitTree()
+
+        # Add each entry to our new tree, in turn
+        for entry in contents[path]:
+            # An entry can be a normal GitIndexEntry read from the
+            # index, or a tree we've created.
+            if isinstance(entry, GitIndexEntry): # Regular entry (a file)
+
+                # We transcode the mode: the entry stores it as integers,
+                # we need an octal ASCII representation for the tree.
+                leaf_mode = f"{entry.mode_type:02o}{entry.mode_perms:04o}".encode("ascii")
+                leaf = GitTreeLeaf(mode = leaf_mode, path=os.path.basename(entry.name), sha=entry.sha)
+            else: # Tree.  We've stored it as a pair: (basename, SHA)
+                leaf = GitTreeLeaf(mode = b"040000", path=entry[0], sha=entry[1])
+
+            tree.items.append(leaf)
+
+        # Write the new tree object to the store.
+        sha = object_write(tree, repo)
+
+        # Add the new tree hash to the current dictionary's parent, as
+        # a pair (basename, SHA)
+        parent = os.path.dirname(path)
+        base = os.path.basename(path) # The name without the path, eg main.go for src/main.go
+        contents[parent].append((base, sha))
+
+    return sha
+
+def commit_create(repo, tree, parent, author, timestamp, message):
+    commit = GitCommit() # Create the new commit object.
+    commit.kvlm[b"tree"] = tree.encode("ascii")
+    if parent:
+        commit.kvlm[b"parent"] = parent.encode("ascii")
+
+    # Trim message and add a trailing \n
+    message = message.strip() + "\n"
+    # Format timezone
+    offset = int(timestamp.astimezone().utcoffset().total_seconds())
+    hours = offset // 3600
+    minutes = (offset % 3600) // 60
+    tz = "{}{:02}{:02}".format("+" if offset > 0 else "-", hours, minutes)
+
+    author = author + timestamp.strftime(" %s ") + tz
+
+    commit.kvlm[b"author"] = author.encode("utf8")
+    commit.kvlm[b"committer"] = author.encode("utf8")
+    commit.kvlm[None] = message.encode("utf8")
+
+    return object_write(commit, repo)
+
+
+def cmd_commit(args):
+    repo = repo_find()
+    index = index_read(repo)
+    # Create trees, grab back SHA for the root tree.
+    tree = tree_from_index(repo, index)
+
+    # Create the commit object itself
+    commit = commit_create(repo,
+                           tree,
+                           object_find(repo, "HEAD"),
+                           gitconfig_user_get(gitconfig_read()),
+                           datetime.now(),
+                           args.message)
+
+    # Update HEAD so our commit is now the tip of the active branch.
+    active_branch = branch_get_active(repo)
+    if active_branch: # If we're on a branch, we update refs/heads/BRANCH
+        with open(repo_file(repo, os.path.join("refs/heads", active_branch)), "w") as fd:
+            fd.write(commit + "\n")
+    else: # Otherwise, we update HEAD itself.
+        with open(repo_file(repo, "HEAD"), "w") as fd:
+            fd.write("\n")
