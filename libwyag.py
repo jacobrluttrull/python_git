cafb7b9e897e16852b519cee2eeac2bc88272604
jacobrluttrull <jacobrluttrull@gmail.com>
2026-01-05 20:03:50 -0500

    8 is done now to 9 the last section

--- libwyag.py
+++ libwyag.py
@@ -998,3 +998,116 @@ def check_ignore(rules, path):
         return result
 
     return check_ignore_absolute(rules.absolute, path)
+
+#8.5 Status Command
+argsp = argsubparsers.add_parser("status", help="Show the working tree status.")
+
+
+
+def cmd_status(args):
+    repo = repo_find()
+    index = index_read(repo)
+
+    cmd_status_branch(repo)
+    cmd_status_head_index(repo, index)
+    print()
+    cmd_status_index_worktree(repo, index)
+
+def branch_get_active(repo):
+    with open(repo_file(repo, "HEAD"), "r") as f:
+        head = f.read().strip()
+
+    if head.startswith("ref: refs/heads/ "):
+        return head[16:-1]
+    else:
+        return False
+def cmd_status_branch(repo):
+    branch = branch_get_active(repo)
+    if branch:
+        print(f"On branch {branch}")
+    else:
+        print("Not on any branch.")
+def tree_to_dict(repo, ref, prefix=""):
+    ret = dict()
+    tree_sha = object_read(repo, ref, fmt=b"tree")
+    tree = object_read(repo, tree_sha)
+
+    for leaf in tree.items:
+        full_path = os.path.join(prefix, leaf.path)
+
+        is_subtree = (leaf.mode.startswith(b"04"))
+
+        if is_subtree:
+            ret.update(tree_to_dict(repo, leaf.sha, full_path))
+        else:
+            ret[full_path] = leaf.sha
+    return ret
+def cmd_status_head_index(repo, index):
+    print("Changes to be committed:")
+    head = tree_to_dict(repo, "HEAD")
+    for entry in index.entries:
+        if entry.name in head:
+            if entry.sha != head[entry.name]:
+                print(f"  modified: {entry.name}")
+            del head[entry.name]
+        else:
+            print(f"  new file: {entry.name}")
+    for entry in head.keys():
+        print(f"  deleted: {entry}")
+
+def cmd_status_index_worktree(repo, index):
+    print("Changes not staged for commit:")
+
+    ignore = gitignore_read(repo)
+
+    gitdir_prefix = repo.gitdir + os.path.sep
+
+    all_files = list()
+
+    # We begin by walking the filesystem
+    for (root, _, files) in os.walk(repo.worktree, True):
+        if root == repo.gitdir or root.startswith(gitdir_prefix):
+            continue
+        for f in files:
+            full_path = os.path.join(root, f)
+            rel_path = os.path.relpath(full_path, repo.worktree)
+            all_files.append(rel_path)
+
+    # We now traverse the index, and compare real files with the cached
+    # versions.
+
+    for entry in index.entries:
+        full_path = os.path.join(repo.worktree, entry.name)
+
+        # That file *name* is in the index
+
+        if not os.path.exists(full_path):
+            print("  deleted: ", entry.name)
+        else:
+            stat = os.stat(full_path)
+
+            # Compare metadata
+            ctime_ns = entry.ctime[0] * 10 ** 9 + entry.ctime[1]
+            mtime_ns = entry.mtime[0] * 10 ** 9 + entry.mtime[1]
+            if (stat.st_ctime_ns != ctime_ns) or (stat.st_mtime_ns != mtime_ns):
+                # If different, deep compare.
+                # @FIXME This *will* crash on symlinks to dir.
+                with open(full_path, "rb") as fd:
+                    new_sha = object_hash(fd, b"blob", None)
+                    # If the hashes are the same, the files are actually the same.
+                    same = entry.sha == new_sha
+
+                    if not same:
+                        print("  modified:", entry.name)
+
+        if entry.name in all_files:
+            all_files.remove(entry.name)
+
+    print()
+    print("Untracked files:")
+
+    for f in all_files:
+        # @TODO If a full directory is untracked, we should display
+        # its name without its contents.
+        if not check_ignore(ignore, f):
+            print(" ", f)
